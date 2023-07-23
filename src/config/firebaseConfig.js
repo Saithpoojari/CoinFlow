ef586b1582ad1ec093dd7e3e8d5b30ad551d8d10
saith poojari <108398278+Saithpoojari@users.noreply.github.com>
2023-07-23 15:15:56 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfUyQeruLVp3nVs8F29L_404-3PRPE2dU",
+  authDomain: "coin-flow-b0776.firebaseapp.com",
+  projectId: "coin-flow-b0776",
+  storageBucket: "coin-flow-b0776.appspot.com",
+  messagingSenderId: "673401426375",
+  appId: "1:673401426375:web:8bd3a97289faa39806c9e2"
+};
+
+export default firebaseConfig;
+ 
